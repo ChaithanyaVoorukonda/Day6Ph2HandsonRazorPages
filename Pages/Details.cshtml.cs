4d9552020a5c01acf7471da83d5c804763cbf6ea
ChaithanyaVoorukonda <138677532+ChaithanyaVoorukonda@users.noreply.github.com>
2025-03-11 10:19:50 +0530

    Day6Handson_RazorPages

---
+++ Pages/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ExampleRazor.Models;
+
+namespace ExampleRazor.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ExampleRazor.Models.AppDbContext _context;
+
+        public DetailsModel(ExampleRazor.Models.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Customer Customer { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FirstOrDefaultAsync(m => m.CustomerID == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Customer = customer;
+            }
+            return Page();
+        }
+    }
+}
